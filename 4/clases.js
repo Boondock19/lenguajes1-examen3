0935df3a74213d335878ebfb9ba7ac21d5fdc68d
Boondock19 <josematiasgonzalez20@gmail.com>
2022-07-31 16:53:17 -0400

    Funcion fatherExist funciona, falta la verificacion de no tener repetidas los metodos en una declaracion

--- 4/clases.js
+++ 4/clases.js
@@ -1,82 +1,70 @@
-
-import { SimpleClass , SuperClass} from './typesClasses.js'
-
+import { SimpleClass, SuperClass } from "./typesClasses.js";
 
 console.log("Bienvenido al programa manejador de clases");
-console.log('Luego de cada accion, se pedira una siguiente')
+console.log("Luego de cada accion, se pedira una siguiente");
 
 // Creando un array para cada uno de los programas a definir
 
 /**
  * Array de clases que heredan de una super clase
- * 
+ *
  */
-let arrayOfClasses = []
+let arrayOfClasses = [];
 
 /**
  * Array de clases que no heredan de una super clase
- * 
+ *
  */
 
-let arrayOfSuperClass = []
+let arrayOfSuperClass = [];
 
-let arrayOfNames = []
+let arrayOfNames = [];
 
-process.stdin.on('data', data => {
+process.stdin.on("data", (data) => {
+  const dataString = data.toString();
+  const dataArray = dataString.split(" ");
 
-    const dataString = (data).toString()
-    const dataArray =dataString.split(" ")
-   
-    /* 
+  /* 
         Creacion de tipos atomicos
     */
-    if(dataArray[0].trim() == "CLASS") {
-        if (dataArray.length < 3 ) {
-            console.log("Error: faltan datos")
-        } else {
-            console.log("Entro en CLASS")
-            console.log(dataArray)
-            let expression = dataArray[2].trim()
-            if (expression == ':' && dataArray.length < 5) {
-                console.log("Error: faltan datos para declarar una herencia, recuerde que la clase debe definir al menos un metodo")
-            } else {
-                defType(dataArray)
-                console.log('Array of clases', arrayOfClasses)
-                console.log('array of superClases',arrayOfSuperClass)
-            }
-           
-        }
-       
-        
+  if (dataArray[0].trim() == "CLASS") {
+    if (dataArray.length < 3) {
+      console.log("Error: faltan datos");
+    } else {
+      console.log("Entro en CLASS");
+      console.log(dataArray);
+      let expression = dataArray[2].trim();
+      if (expression == ":" && dataArray.length < 5) {
+        console.log(
+          "Error: faltan datos para declarar una herencia, recuerde que la clase debe definir al menos un metodo"
+        );
+      } else {
+        defType(dataArray);
+        console.log("Array of clases", arrayOfClasses);
+        console.log("array of superClases", arrayOfSuperClass);
+      }
     }
-        
-    
+  }
 
-        
-    
-
-    /* 
+  /* 
         Creacion de structs
     */
-    if(dataArray[0].trim() == "DESCRIBIR") {
-        if (dataArray.length < 2 ) {
-            console.log("Error: faltan datos")        
-
-        } else {
-            console.log("Entro en DESCRIBIR")
-            console.log(dataArray)     
-        }
+  if (dataArray[0].trim() == "DESCRIBIR") {
+    if (dataArray.length < 2) {
+      console.log("Error: faltan datos");
+    } else {
+      console.log("Entro en DESCRIBIR");
+      console.log(dataArray);
     }
+  }
 
- 
-    console.log("Siguiente accion: ")
+  console.log("Siguiente accion: ");
 
-    // Salimos del programa
-    if (dataArray[0].trim() === 'SALIR') {
-        console.log('Terminando el programa ...')
-        process.exit()
-    }
-    
+  // Salimos del programa
+  if (dataArray[0].trim() === "SALIR") {
+    console.log("Terminando el programa ...");
+    process.exit();
+  }
 });
 
 /**
@@ -86,48 +74,62 @@ process.stdin.on('data', data => {
  */
 
 const defType = (types) => {
-    console.log("Entro en defType")
-    console.log(types)
-    let methods = []
-    let name = ''
-    if (types.includes(':')) {
-        name = types[1]
-        if (nameExists(name)) {
-            console.log("Error: el nombre ya existe")
-        } else { 
-            let father = types[3]
-            types[types.length - 1] = types[types.length - 1].trim()
-            methods = types.slice(4)
-            const newClass = new SimpleClass(name,methods,father)
-            arrayOfClasses.push(newClass)
-            arrayOfNames.push(name)
-            // console.log("Contiene :, clase que hereda de una super clase")
-        }
+  console.log("Entro en defType");
+  console.log(types);
+  let methods = [];
+  let name = "";
+  if (types.includes(":")) {
+    name = types[1];
+    if (nameExists(name, arrayOfNames)) {
+      console.log("Error: el nombre ya existe");
     } else {
-        name = types[1]
-        if (nameExists(name)) {
-            console.log("Error: el nombre ya existe")
-        } else {
-            types[types.length - 1] = types[types.length - 1].trim()
-            methods = types.slice(3)
-            const newSuperClass = new SuperClass(name,methods)
-            arrayOfSuperClass.push(newSuperClass)
-            arrayOfNames.push(name)
-            // console.log("No contiene : es una clase con metodos")
-        }
+      let father = types[3];
+      if (!fatherExists(father, arrayOfSuperClass)) {
+        console.log("Error: el padre no existe");
+      } else {
+        types[types.length - 1] = types[types.length - 1].trim();
+        methods = types.slice(4);
+        const newClass = new SimpleClass(name, methods, father);
+        arrayOfClasses.push(newClass);
+        arrayOfNames.push(name);
+        // console.log("Contiene :, clase que hereda de una super clase")
+      }
     }
-    
-}
+  } else {
+    name = types[1];
+    if (nameExists(name, arrayOfNames)) {
+      console.log("Error: el nombre ya existe");
+    } else {
+      types[types.length - 1] = types[types.length - 1].trim();
+      methods = types.slice(3);
+      const newSuperClass = new SuperClass(name, methods);
+      arrayOfSuperClass.push(newSuperClass);
+      arrayOfNames.push(name);
+      // console.log("No contiene : es una clase con metodos")
+    }
+  }
+};
 
+const nameExists = (name, array) => {
+  let exist = array.find((element) => {
+    if (element == name) {
+      return name;
+    } else {
+      return undefined;
+    }
+  });
 
-const nameExists = (name) => {
-    let exist = arrayOfNames.find((element) => {
-        if (element == name) {
-            return name
-        } else {
-            return undefined
-        } 
-    })
+  return exist;
+};
 
-    return exist
-}
+const fatherExists = (name, array) => {
+    let exist = array.find((element) => {
+      if (element.name == name) {
+        return name;
+      } else {
+        return undefined;
+      }
+    });
+  
+    return exist;
+  };
