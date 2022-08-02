1aed9b3385476c8ca2f617ef9964c1b528d2e6c5
Boondock19 <josematiasgonzalez20@gmail.com>
2022-08-01 22:11:35 -0400

    Realiza implementacion de DFS con Pila

--- 1/DFS.kt
+++ 1/DFS.kt
@@ -1,4 +1,5 @@
 package ve.usb.libGrafo
+import estructruras.secuencias.*
 
  /*
      descripcion: Clase  que retorna el DFS de un grafo g 
@@ -11,71 +12,47 @@ package ve.usb.libGrafo
         se recorren los adyacentes  en la ejecucion del mismo.
 
      */
-public class DFS(val g: Grafo, val orden: MutableList<Int> = mutableListOf<Int>()) {
+public class DFS(val g: Grafo, val d:Int,val h:Int) : Busqueda<Int>() {
     
     var tiempo = 0
     var vertices = mutableListOf<Int>()  
-    var listaDeLados : MutableList<MutableList<Lado>> = mutableListOf() // idea para el arbol dps
     var numVertices = g.obtenerNumeroDeVertices()
     var predecesores = Array(numVertices) {(Integer.MAX_VALUE)}
     var predecesoresNull = arrayOfNulls <Int?>(numVertices)
     var tiempoInicial = Array(numVertices) {(Integer.MAX_VALUE)}
     var tiempoFinal = Array(numVertices) {(Integer.MAX_VALUE)}
     var colores = Array(numVertices) {Color.BLANCO}
-    var contadorComponenteConexa = Array(numVertices) {mutableListOf<Int>()}
-    var contador = 0
-    var ordenTopologicoLista = mutableListOf<Int>()
-    var contadorComponenteConexaVertice = Array(numVertices) {0} // obtener contador de componente conexa directamente
-     
+    var stack = Pila<Int>()
+    var contar = true
+    var verticesExplorados = 0
+
+
     init {
 	// Se ejecuta DFS
         for( i in 0..this.numVertices-1) {
             this.vertices.add(i)
         }
-
-
-        if (orden.size > 0 ) {
-            this.vertices.forEachIndexed {index,v -> 
-                if (this.colores[v] === Color.BLANCO) {
-                    dfsVisit(g,orden[index])
-                }
-            }
-        } else {
-
-            this.vertices.forEach { v ->
-            if (this.colores[v] === Color.BLANCO) {
-                dfsVisit(g,v)
-                this.contador ++ // cada vez que entre en este if, corresponde a una componenteConexa
-            }
-            }
-        } 
-    }
-
-    private fun dfsVisit(g: Grafo, u: Int) {
-
         this.tiempo = this.tiempo + 1
-        this.tiempoInicial[u] = tiempo
-        this.colores[u] = Color.GRIS
-        this.contadorComponenteConexa[this.contador].add(u)
-        this.contadorComponenteConexaVertice[u] = this.contador
-        g.adyacentes(u).forEach { l ->
-            var v = l.elOtroVertice(u)
-            if (this.colores[v] === Color.BLANCO) {
-              //  this.predecesores[v] = u
-                this.predecesoresNull[v] = u
-                dfsVisit(g,v)
+        this.tiempoInicial[d] = this.tiempo 
+        this.stack.agregar(d)
+        this.colores[d] = Color.NEGRO
+
+        while (!this.stack.vacio()) {
+            val u = this.stack.remover()
+            if (this.contar) this.verticesExplorados++
+            g.adyacentes(u).forEach { l -> 
+                var v = l.elOtroVertice(u)
+                if (v == h) this.contar = false
+                if (this.colores[v] === Color.BLANCO) {
+                    this.stack.agregar(v)
+                    this.predecesoresNull[v] = u
+                    this.colores[v] = Color.NEGRO
+                }
             }
         }
 
-        this.colores[u] = Color.NEGRO // se termina explorar V
-        this.tiempo = this.tiempo + 1
-        this.tiempoFinal[u] = this.tiempo
-        // Lista de orden topologico invertida (El primer que termina se agrega a la lista,pero la podemor voltear)
-        this.ordenTopologicoLista.add(u)
-
-       // println("Predecesores con infinitos : ${this.predecesores.joinToString()} \n")
-       // println("Predecesores con nulls : ${this.predecesoresNull.joinToString()} \n")
     }
+
 
       /*
         descripcion: Funcion que retorna al predecesor del vertice v
@@ -164,82 +141,13 @@ public class DFS(val g: Grafo, val orden: MutableList<Int> = mutableListOf<Int>(
         return camino
      }
 
-     /*
-        descripcion: Funcion que retorna el camino  desde un vertice u hasta v
-         
-       precondiciones: que sea invocado por un objeto de la clase DFS 
-
-        postcondiciones: lista que representa al camino  desde u hasta v
-
-        tiempo de la operacion: O(v) ya que se recorren los vertices para obtener el camino.
-
-     */
     
-    fun caminoDesdeHasta(u: Int, v: Int) : Iterable<Int>  {  
-        if (!(v in this.vertices) || !(u in this.vertices)) {
-            throw RuntimeException("El vertice ${v} o $u no se encuentra en el grafo")
+    override fun buscar(d:Int,h:Int) : Int {
+        if (this.contar) {
+            return -1
+        } else {
+            return this.verticesExplorados
         }
-        
-
-        var camino = mutableListOf<Int>()
-        var direccionBusqueda = this.comparacionTiempo(u,v)
-       
-        if (!(this.hayCamino(u,v))) throw RuntimeException("No existe un camino desde $u hasta $v")
-
-        
-        // si es U entonces el sucesor/hijo es v
-        if (direccionBusqueda === u ) {
-            var verticeBuscarPredecesor = v
-            camino.add(v)
-            while (this.obtenerPredecesor(verticeBuscarPredecesor) != u) {
-            camino.add(this.obtenerPredecesor(verticeBuscarPredecesor) as Int)
-            verticeBuscarPredecesor = this.obtenerPredecesor(verticeBuscarPredecesor) as Int
-            }
-            // al salir del while debemos agregar a u
-            camino.add(u)
-        } 
-
-        // si es v entonces el sucesor/hijo es u
-        if (direccionBusqueda === v) {
-            var verticeBuscarPredecesor = u
-            camino.add(u)
-            while (this.obtenerPredecesor(verticeBuscarPredecesor) != v) {
-            camino.add(this.obtenerPredecesor(verticeBuscarPredecesor) as Int)
-            verticeBuscarPredecesor = this.obtenerPredecesor(verticeBuscarPredecesor) as Int
-            }
-            // al salir del while debemos agregar a v
-            camino.add(v)
-        }
-       
-        return camino.reversed() 
-
-
     }
-    
-
-       /*
-        descripcion: Funcion que retorna el bosque generado por el DFS
-         
-       precondiciones: que sea invocado por un objeto de la clase DFS
-
-        postcondiciones: Array en el que cada slot que representa a un arbol del DFS (componente Conexa)
-
-        tiempo de la operacion: O(1) ya que es una asignacion
-
-     */
-
-   fun depthFirstForest() :List<MutableList<Int>>{ 
-       // La unica manera que se me ocurre para poder mostrar todos los Arboles
-        // es a traves de un array de listas mutables donde cada lista representa a
-        // una componente conexa y que por lo tanto es un arbol producto del DFS
-
-        // filter devuelve una Lista
-       return this.contadorComponenteConexa.filter { it.size > 0 }
-   }
-
-   fun obtenerOrdenTopologico() : Iterable<Int> {
-       var ordenTopologicoListaInversa = this.ordenTopologicoLista.reversed()
-       return ordenTopologicoListaInversa
-   }
 
 }
--- 1/MainBusqueda.kt
+++ 1/MainBusqueda.kt
@@ -6,12 +6,19 @@ fun main(args: Array<String>) {
     var grafoNoDirigido = GrafoNoDirigido(7)
 
     // Agregando aristas al grafo
-    grafoNoDirigido.agregarArista(Arista(0,2))
-    grafoNoDirigido.agregarArista(Arista(2,3))
-    grafoNoDirigido.agregarArista(Arista(3,1))
-    grafoNoDirigido.agregarArista(Arista(3,6))
-    grafoNoDirigido.agregarArista(Arista(6,5))
-    grafoNoDirigido.agregarArista(Arista(3,4))
+    // grafoNoDirigido.agregarArista(Arista(0,2))
+    // grafoNoDirigido.agregarArista(Arista(2,3))
+    // grafoNoDirigido.agregarArista(Arista(3,1))
+    // grafoNoDirigido.agregarArista(Arista(3,6))
+    // grafoNoDirigido.agregarArista(Arista(6,5))
+    // grafoNoDirigido.agregarArista(Arista(3,4))
+
+
+    grafoNoDirigido.agregarArista(Arista(0,1))
+    grafoNoDirigido.agregarArista(Arista(1,3))
+    grafoNoDirigido.agregarArista(Arista(3,5))
+    grafoNoDirigido.agregarArista(Arista(2,4))
+    
     
     
     
@@ -20,9 +27,19 @@ fun main(args: Array<String>) {
     // hay camino desde 0 a 5?
     /*
         BFS solicita el grafo, el vertice D y el vertice H
+        comienza el BFS desde el vertice D 
 
      */
     var bfs = BFS(grafoNoDirigido, 0, 5)
     println("buscar con BFS ,  ${bfs.buscar(0,5)}")
+
+    // hay camino desde 0 a 5?
+    /*
+        DFS solicita el grafo, el vertice D y el vertice H
+        comienza el DFS desde d
+     */
+
+     var dfs = DFS(grafoNoDirigido, 0, 5)
+     println("buscar con DFS ,  ${dfs.buscar(0,5)}")
 
 }
